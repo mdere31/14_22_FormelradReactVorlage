c64a7496b51c825522c19dae628b289aa039f412
Mehmet Nadir Dere <mehmet.dere@lernende.bbw.ch>
2025-06-10 12:29:43 +0200

    A-message-field-is-required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -8,10 +8,11 @@ export default function Formelrad() {
         u: 10,
         i: 2,
         r: "",
-        p: ""
+        p: "",
+        message: ""
     })
 
- const handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         console.log("handleSubmit")
         if (values.u === "" && values.i === "") {
@@ -40,6 +41,7 @@ export default function Formelrad() {
             setValues(values => ({...values, p: values.u * values.i}));
         }
     }
+
     return (
         <>
             <section>
@@ -53,6 +55,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <p>{values.message}</p>
                 </form>
             </section>
         </>
